731fe549a981045503a65593644ea0305fb68bb5
Harika Naishadham <harikanaishadham@gmail.com>
2023-06-19 12:09:19 +0530

    Time: 447 ms (94.42%), Space: 17.7 MB (23.60%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.py
@@ -0,0 +1,8 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        lastPos=len(nums)-1
+        
+        for i in range(len(nums)-2,-1,-1):
+            if i+nums[i]>=lastPos:
+                lastPos=i
+        return lastPos==0
