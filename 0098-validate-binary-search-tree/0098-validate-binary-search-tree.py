731b1239a71de2ebde6d0e6265ee6c3a782eb413
Harika Naishadham <harikanaishadham@gmail.com>
2023-06-28 13:17:36 +0530

    Time: 49 ms (96.44%), Space: 18.8 MB (86.56%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -4,21 +4,16 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        if not root:
-            return True
         
-        def inOrder(root, order):
-            if root is None:
-                return
-            inOrder(root.left, order)
-            order.append(root.val)
-            inOrder(root.right, order)
-            
-        order = []
-        inOrder(root, order)
-        for i in range(1, len(order)):
-            if order[i] <= order[i-1]:
+        def helper(node, low, high):
+            if not node:
+                return True
+            if not (low < node.val < high):
                 return False
-        return True
+            return helper(node.left, low, node.val) and helper(node.right, node.val, high)
+        
+        return helper(root, -inf, inf)
+    
