4103f72dc032c36821aaa30dbc22fc3b0844d22f
Harika Naishadham <harikanaishadham@gmail.com>
2023-07-20 13:04:51 +0530

    Time: 183 ms (22.12%), Space: 31.6 MB (70.04%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -9,7 +9,6 @@ class Solution:
         l1,l2=headA,headB
         while l1!=l2:
             l1=l1.next if l1 else headB
-            
             l2=l2.next if l2 else headA
         return l1
-        
+            
