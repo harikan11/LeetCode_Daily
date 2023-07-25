529f6bc0a2ff026d7724a8ca912eb98b49bb064c
Harika Naishadham <harikanaishadham@gmail.com>
2023-07-25 18:59:10 +0530

    Time: 49 ms (63.68%), Space: 16.6 MB (48.74%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,23 +1,22 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res=[]
-        nums.sort()
-        def backtrack(i,subset):
-            if i==len(nums):
-                res.append(subset[::])
+        
+        subsets=[]
+        def dfs(i):
+            if i>=len(nums):
+                res.append(subsets.copy())
                 return
+            #decision to include the number
+            subsets.append(nums[i])
+            dfs(i+1)
             
-            #to select an element in the subset
-            subset.append(nums[i])
-            backtrack(i+1,subset)
-            subset.pop()
-            
-            
-            
-            
-            #to not select an element in the subset
-            while i+1<len(nums) and nums[i]==nums[i+1]:
-                i+=1
-            backtrack(i+1,subset)
-        backtrack(0,[])
+            #decision to not include the number
+            subsets.pop()
+            dfs(i+1)
+        dfs(0)
         return res
+            
+            
+            
+            
