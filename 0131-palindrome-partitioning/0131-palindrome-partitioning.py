c7cb8a70feeaa6323ed3d8bd7c87bfbf2d880f49
Harika Naishadham <harikanaishadham@gmail.com>
2023-07-25 22:00:18 +0530

    Time: 656 ms (69.74%), Space: 33.8 MB (26.30%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,17 +1,23 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        result = []
-
-        def backtrace(cur, ind):
-            if ind == len(s):
-                result.append(cur)
+        res=[]
+        part=[]
+        def dfs(i):
+            if i>=len(s):
+                res.append(part.copy())
                 return
+            for j in range(i,len(s)): #iterating through every possible substring
+                if self.isPali(s,i,j):
+                    part.append(s[i:j+1])
+                    dfs(j+1)
+                    part.pop()
+                    
+        dfs(0)
+        return res
+    def isPali(self,s,l,r):
+        while l<r:
+            if s[l]!=s[r]:
+                return False
+            l,r=l+1,r-1
+        return True
             
-            temp = ""
-
-            for i in range(ind, len(s)):
-                temp += s[i]
-                if temp == temp[::-1]:
-                    backtrace(cur + [temp], i + 1)
-        backtrace([], 0)
-        return result
