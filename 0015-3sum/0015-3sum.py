b766fcc6d1dfcaf215763f24570de30030d2e12a
Harika Naishadham <harikanaishadham@gmail.com>
2023-07-31 18:51:17 +0530

    Time: 988 ms (94.32%), Space: 20.3 MB (88.85%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,32 +1,27 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()  # Sort the input array in ascending order
-        result = []
-        
-        for i in range(len(nums) - 2):
-            if i > 0 and nums[i] == nums[i-1]:
-                continue  # Skip duplicate elements
-            
-            left = i + 1
-            right = len(nums) - 1
-            
-            while left < right:
-                current_sum = nums[i] + nums[left] + nums[right]
-                
-                if current_sum < 0:
-                    left += 1
-                elif current_sum > 0:
-                    right -= 1
+        res=[]
+        nums.sort()
+        #every number in the array is a possible first value
+        for i,a in enumerate(nums):
+            #you dont want to use the same value in the same pos of the res twice
+            if i>0 and a==nums[i-1]:  #a==nums[i-1] checks if the next value is same as the nums[i-1]
+                continue
+            l=i+1
+            r=len(nums)-1
+            while l<r:
+                threeSum=a+nums[l]+nums[r]
+                if threeSum>0:
+                    r-=1
+                elif threeSum<0:
+                    l+=1
                 else:
-                    result.append([nums[i], nums[left], nums[right]])
+                    res.append([a,nums[l],nums[r]])
+                    l+=1
+                    while nums[l]==nums[l-1] and l<r:
+                        l+=1
+        return res
                     
-                    # Skip duplicate elements
-                    while left < right and nums[left] == nums[left+1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right-1]:
-                        right -= 1
                     
-                    left += 1
-                    right -= 1
-        
-        return result
+                
+                    
