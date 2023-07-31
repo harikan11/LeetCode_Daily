5a9a9e29d50f69c5f89f7ec3a516706e470a11eb
Harika Naishadham <harikanaishadham@gmail.com>
2023-07-31 19:35:32 +0530

    Time: 576 ms (82.81%), Space: 16.6 MB (19.04%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,31 +1,33 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         nums.sort()
-        n = len(nums)
-        result = []
-        for i in range(n - 3):
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
-            for j in range(i + 1, n - 2):
-                if j > i + 1 and nums[j] == nums[j - 1]:
-                    continue
-                l, r = j + 1, n - 1
-                while l < r:
-                    s = nums[i] + nums[j] + nums[l] + nums[r]
-                    if s == target:
-                        result.append([nums[i], nums[j], nums[l], nums[r]])
-                        while l < r and nums[l] == nums[l + 1]:
-                            l += 1
-                        while l < r and nums[r] == nums[r - 1]:
-                            r -= 1
-                        l += 1
-                        r -= 1
-                    elif s < target:
-                        l += 1
-                    else:
-                        r -= 1
-        return result
-
-        
+        res=[] #final res of quadruplets
+        quad=[] #curr quadruplets that we're building on
+        def kSum(k,start,target): #nums is not gonna change so we dont have to define it in the helper function but target is defined because its changing 
+            if k!=2:
+                for i in range(start,len(nums)-k+1):
+                    if i>start and nums[i]==nums[i-1]:
+                        continue
+                    quad.append(nums[i])
+                    kSum(k-1,i+1,target-nums[i])
+                    quad.pop()
+                return
+            
+            #base case
+            l=start
+            r=len(nums)-1
+            while l<r:
+                if nums[l]+nums[r]<target:
+                    l+=1
+                elif nums[l]+nums[r]>target:
+                    r-=1
+                else:
+                    res.append(quad+[nums[l],nums[r]])
+                    l+=1
+                    while l<r and nums[l]==nums[l-1]:
+                        l+=1
+        kSum(4,0,target)
+        return res
                 
-        
+                
+            
