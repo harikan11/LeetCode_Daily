36c6d61c9cb943cb2b8c3fcd3552a00ce8a0d371
Harika Naishadham <harikanaishadham@gmail.com>
2023-08-02 23:53:12 +0530

    Time: 123 ms (97.41%), Space: 17.8 MB (20.15%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,7 +1,6 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        missing=len(nums)
-        for i in range(len(nums)):
-            missing^=i^nums[i]
-        return missing
-        
+        n=len(nums)
+        for i, num in enumerate(nums):
+            n^=i^num
+        return n
