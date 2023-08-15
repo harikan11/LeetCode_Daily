71c2c20e632d2c7d6e48d3efa3844377b8f69e08
Harika Naishadham <harikanaishadham@gmail.com>
2023-08-15 12:04:17 +0530

    Time: 43 ms (86.20%), Space: 17.7 MB (9.28%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.py
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.py
@@ -9,17 +9,24 @@ class Solution:
         """
         Do not return anything, modify root in-place instead.
         """
-        if not root:
-            return None
-        stack=[root]
-        while stack:
-            node=stack.pop()
-            if node.right:
-                stack.append(node.right)
-            if node.left:
-                stack.append(node.left)
-            if len(stack)>0:
-                node.right=stack[-1]
-            node.left=None
-                
+        def dfs(root): #this function returns the tail 
+            if not root:
+                return None #cant flatten empty tree
+            #recursive case
+            #first on left subtree
+            leftTail= dfs(root.left)
+            
+            #recursive dfs on right subtree
+            rightTail=dfs(root.right)
+            
+            #if both the subtrees are empty, no need to do anything since nothing can be added
+            #if left subtree is empty, then root and right subtree are already flattened
+            #only one condition left where left subtree is not empty
+            if root.left:
+                leftTail.right=root.right #lefttail will point to the curr right child of root
+                root.right=root.left
+                root.left=None
+            last=rightTail or leftTail or root
+            return last
+        dfs(root)
                 
