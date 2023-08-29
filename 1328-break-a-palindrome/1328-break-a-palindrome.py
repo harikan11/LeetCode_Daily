d00def311517ecf528028ea5a31aa5ee00cc286d
Harika Naishadham <harikanaishadham@gmail.com>
2023-08-29 11:54:06 +0530

    Time: 37 ms (77.70%), Space: 16.3 MB (27.70%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.py
@@ -0,0 +1,17 @@
+class Solution:
+
+    def breakPalindrome(self, palindrome: str) -> str:
+        if len(palindrome) == 1:
+            return ''
+
+        n = len(palindrome)
+        letters = list(palindrome)
+
+        for i in range(n // 2):
+            if letters[i] > 'a':
+                letters[i] = 'a'
+                break
+        else:
+            letters[-1] = 'b'
+
+        return ''.join(letters)
