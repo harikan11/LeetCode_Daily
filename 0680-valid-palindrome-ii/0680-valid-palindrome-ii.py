ec23b2ee7f4062023bef38eecea042da838e7845
Harika Naishadham <harikanaishadham@gmail.com>
2023-08-29 11:00:54 +0530

    Time: 81 ms (94.01%), Space: 17 MB (39.70%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -1,8 +1,12 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        def validPal(s, flag):
-            if len(s) < 2: return True
-            if s[0] == s[-1]: return validPal(s[1:-1], flag)
-            if flag: return validPal(s[1:], False) or validPal(s[:-1], False)
-            return False
-        return validPal(s, True)
+            p1=0
+            p2=len(s)-1
+            while p1<=p2:
+                if s[p1]!=s[p2]:
+                    string1=s[:p1]+s[p1+1:]
+                    string2=s[:p2]+s[p2+1:]
+                    return string1==string1[::-1] or string2==string2[::-1]
+                p1+=1
+                p2-=1
+            return True
