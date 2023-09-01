b17dbce6cd3201c865b799a7599bb63d2c52862c
Harika Naishadham <harikanaishadham@gmail.com>
2023-09-01 19:35:20 +0530

    Time: 84 ms (86.53%), Space: 18.8 MB (46.13%) - LeetHub

---
+++ 1367-linked-list-in-binary-tree/1367-linked-list-in-binary-tree.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubPath(self, head: Optional[ListNode], root: Optional[TreeNode]) -> bool:
+        if not head:
+            return True
+        if not root:
+            return False
+        
+        if head.val==root.val:
+            if self.issame(head,root):
+                return True
+        return self.isSubPath(head,root.left) or self.isSubPath(head,root.right)
+    
+    def issame(self,head,root):
+        if not head:
+            return True
+        if not root:
+            return False
+        if head.val==root.val:
+            return self.issame(head.next,root.left) or self.issame(head.next,root.right)
+        return False
